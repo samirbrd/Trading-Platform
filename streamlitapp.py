878aca3d7d585ac328f36ed5c858626f7826ab03
samirbrd <39365800+samirbrd@users.noreply.github.com>
2023-08-29 21:40:07 +0530

    Add files via upload

--- streamlitapp.py
+++ streamlitapp.py
@@ -13,12 +13,12 @@ import pandas as pd
 
 st.title('Position Size BankNifty')
 #st.write("Here's my first attempt at writing some text")
-path_string_1=""./""
-file_string_1=path_string_1+'strategyposition.csv'
+#path_string_1=""./""
+file_string_1='strategyposition.csv'
 df_position=pd.read_csv(file_string_1)
-file_string_2=pathString_1+"Strategy.csv"
+file_string_2="Strategy.csv"
 df_strategy=pd.read_csv(file_string_2)
-df_position_1=pd.read_csv(path_string_1+'Positions.csv')
+df_position_1=pd.read_csv('Positions.csv')
 df_position=pd.concat([df_position,df_position_1])
 #df_position=df_position.merge(df_strategy,left_on='Strategy',right_on='Strategy')
 #cols=['ID','Strategy','Position','Trade Price','Trade Time','Eval Time','Status']
@@ -53,7 +53,7 @@ total_position=df_filter['Position'].sum()
 
 st.write('Total Bank Nifty Position Size is '+str(total_position))
 st.write(df_filter)
-df_position_summary=pd.read_csv('G:\\My Drive\\Trading\\Position Summary\\positionsummary.csv')
+df_position_summary=pd.read_csv('positionsummary.csv')
 total_m2m=df_position_summary['M2M'].sum()
 st.write(df_position_summary)
 st.write('Total M2M for day:'+str(total_m2m))
