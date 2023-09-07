56d9a3b548c4c98f34874ebd02f6d43c6ccc9ac3
samirbrd <samirbrd@gmail.com>
2023-09-07 07:42:57 +0530

    First Commit

--- streamlitapp.py
+++ streamlitapp.py
@@ -14,7 +14,7 @@ import pandas as pd
 st.title('Position Size BankNifty')
 # #st.write("Here's my first attempt at writing some text")
 path_string_1=
-file_string_1='strategyposition.csv'
+file_string_1='positionsummary.csv'
 df_position=pd.read_csv(file_string_1)
 # file_string_2="Strategy.csv"
 # df_strategy=pd.read_csv(file_string_2)
